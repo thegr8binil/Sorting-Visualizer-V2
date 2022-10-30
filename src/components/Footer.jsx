7f3b9758bb888c3ba65ed273c5051697bc2aa7d4
Binil B <binil.b.re@gmail.com>
2022-10-30 15:03:52 +0530

    commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: -40px;
+  width: 100%;
+`;
+
+export function Footer() {
+  return (
+    <FooterDiv>
+    </FooterDiv>
+  );
+}
